4eb28694b577b484ffc9807da6b5bba62cb223fe
YongWoo-8933 <dyddn2015@gmail.com>
2024-03-02 23:18:49 +0900

    [chore, feat] 1.6.0버전업, 서울외 지역 카페 추가 시도시 문구 표시

--- lib/core/app_version.dart
+++ lib/core/app_version.dart
@@ -1,6 +1,6 @@
 
 class LocalAppVersion {
   static const int major = 1;
-  static const int minor = 5;
-  static const int patch = 3;
+  static const int minor = 6;
+  static const int patch = 0;
 }
